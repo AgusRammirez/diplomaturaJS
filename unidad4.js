a0858b5867e131428aecce6e2a7b9e6a6e0107d0
diplomaturaU3 <agusram6@gmail.com>
2022-09-13 19:40:55 -0300

    Updated

--- unidad4.js
+++ unidad4.js
@@ -1,3 +1,5 @@
+//U4 Parte2
+//Ejercicio 1
 function colorM() {
     var color1 = document.querySelector("body")
     color1.style.backgroundColor = "magenta";
@@ -12,3 +14,5 @@ function colorV() {
     var color3 = document.querySelector("body")
     color3.style.backgroundColor = "green";
 }
+
+//Ejercicio 2
